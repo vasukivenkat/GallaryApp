8b98e4dab2951264a8ff98d1aa6a0a4223eb61dc
vasukivenkat <vasukivenkatv@gmail.com>
2023-01-29 13:02:47 -0800

    controller and views added

---
+++ gallaryapp/Controllers/GallaryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GallaryApp.Controllers
+{
+    public class GallaryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
